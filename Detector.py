526b564093ef1af1a1684ba492a88ad9b9fbc010
Mahdiyeh <Mahdiyeh.ep@gmail.com>
2022-04-24 18:08:09 +0430

    add funcs

--- Detector.py
+++ Detector.py
@@ -1,19 +1,31 @@
-import re
+from hazm import Normalizer
+from hazm import sent_tokenize
+import json
+import codecs
 
 class Detector:
-    #_______________________________________
-    #Mahdiyeh:
-    #Detect vehicles
-    #_______________________________________
+
+    def __init__(self):
+        self.vehicles = json.load(codecs.open(F'Files/vehicles.json', 'r', 'utf-8'))
+        self.stopwords_v = ['با', 'به کمک', 'به وسیله', 'بوسیله', 'با استفاده از', 'بکمک']
 
 
-    # _______________________________________
-    # Shayan:
-    # Detect places
-    # _______________________________________
+    def normalize(self, inputs):
+        normalized = Normalizer().normalize(inputs)
+        samples = sent_tokenize(normalized)
+        return samples
+
+    def find(self, matched]:
+        matched_phrases = []
+        # look for matched phrases
+        if not matched_phrases:
+            return None
+        # return matched
 
 
-    # _______________________________________
-    # Ali:
-    #
-    # _______________________________________
+    def output_generator(self):
+        output = []
+        entities = {0: "from", 1: "to", 2: "vehicle"}
+        for i in range(3):
+        #idk
+        return output
