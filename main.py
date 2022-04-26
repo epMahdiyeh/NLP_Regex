06c114d4365ab9e8c2083fd619304d413960ecc6
Shayan_Jahan <shayantec95@gmail.com>
2022-04-27 00:02:07 +0430

    Improve code

--- main.py
+++ main.py
@@ -11,31 +11,63 @@ def make_regex_str_from_file(file_name):
 
     return result[:-1]
 
-#def normalize(self, inputs):
-    #         normalized = Normalizer().normalize(inputs)
-    #         samples = sent_tokenize(normalized)
-    #         return samples
-
-def print_output(obj, result):
-    print(obj, ":", end=' ')
-    answer = ""
-    if (len(result) > 0):
-        answer = result[0][1]
-    print(answer)
-
 places = make_regex_str_from_file("Files/places.txt")
 vehicles = make_regex_str_from_file("Files/vehicles.txt")
 
-stopwords_f = "از"
-stopwords_t = "به|به سوی|به سمت|به طرف|به مقصد|سمت|تا|در راستای|در جهت"
+stopwords_f = "از|در مسیر"
+stopwords_t = "به|به سوی|به سمت|به طرف|سمت|تا|در راستای|در جهت|آمدم"
 stopwords_v = "با|با یک| با خودروی|به کمک|به وسیله|بوسیله|با استفاده از|بکمک"
 
+def print_output(text, interval):
+    result = ""
+    result_span = ""
+    if (len(text) > 0):
+        result = text[0][1]
+        result_span = [interval[0][0] + len(text[0][0]) + 1, interval[0][1]]
+    return (result, result_span)
+
+def run_on_sentence(sentence: str):
+    interval_from = [(m.start(0), m.end(0)) for m in re.finditer(f"({stopwords_f}) ({places})", sentence)]
+    text_from = re.findall(f"({stopwords_f}) ({places})", sentence)
+
+    interval_to = [(m.start(0), m.end(0)) for m in re.finditer(f"({stopwords_t}) ({places})", sentence)]
+    text_to = re.findall(f"({stopwords_t}) ({places})", sentence)
+
+    interval_vehicle = [(m.start(0), m.end(0)) for m in re.finditer(f"({stopwords_v}) ({vehicles})", sentence)]
+    text_vehicle = re.findall(f"({stopwords_v}) ({vehicles})", sentence)
+
+    fr = print_output(text_from, interval_from)
+    to = print_output(text_to, interval_to)
+    ve = print_output(text_vehicle, interval_vehicle)
+
+    final_result = {
+        "from": fr[0],
+        "from_span": fr[1],
+        "to": to[0],
+        "to_span": to[1],
+        "vehicle": ve[0],
+        "vehicle_span": ve[1]
+    }
+
+    return final_result
+
+def not_empty(dic):
+    for key in dic:
+        if dic[key] != "":
+            return True
+    return False
+
+def run(input: str):
+    sentences = re.split('\.|،|!|\?|!|؟', input)
+    
+    result = []
+    
+    for sentence in sentences:
+        dic = run_on_sentence(sentence)
+        if not_empty(dic):
+            result.append(dic)
+    
+    print(result)
+
 text = input()
-
-result_from = re.findall(f"({stopwords_f}) ({places})", text)
-result_to = re.findall(f"({stopwords_t}) ({places})", text)
-result_vehicle = re.findall(f"({stopwords_v}) ({vehicles})", text)
-
-print_output("from", result_from)
-print_output("to", result_to)
-print_output("vehicle", result_vehicle)
+run(text)
